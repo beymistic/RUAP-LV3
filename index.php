166a5d77f9ee5aabc8f7fe5d96e97e6eb0e6b978
beymistic <tomislav.dm@gmail.com>
2018-05-17 19:42:49 +0200

    nest

--- index.php
+++ index.php
@@ -57,8 +57,7 @@ register.</p>
  </form>
  <?php
 // DB connection info
-// TODO: Update the values for $host, $user, $pwd, and $db //using the values you retrieved
-earlier from the Azure Portal. $host = "value of Data Source";
+// TODO: Update the values for $host, $user, $pwd, and $db //using the values you retrievedearlier from the Azure Portal. $host = "value of Data Source";
 $host = "ruapdbserver.mysql.database.azure.com";
 $user = "bey@ruapdbserver";
 $pwd = "Op3P#UVrpj!";
